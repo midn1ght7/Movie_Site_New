79f63ec361ed64c733cd73ae26f329f542c474e7
midn1ght7 <midn1ght7@users.noreply.github.com>
2022-03-20 14:14:44 +0100

    Primitive login page

--- inz/settings.py
+++ inz/settings.py
@@ -134,3 +134,6 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
